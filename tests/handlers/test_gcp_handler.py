9fbe134d001fff6213a9034d9b1350009a53b8fe
Rakeshkumar-dhanarasu <rakeshkumar@dhanarasu.gmail.com>
2025-06-30 17:20:41 +0530

    Added pipe testing

--- tests/handlers/test_gcp_handler.py
+++ tests/handlers/test_gcp_handler.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from gcphandler import GcpIAMHandler
+from gcp.gcp_iam import GCPIAMHandler
 
 logger = logging.getLogger(__name__)
 
@@ -14,13 +14,13 @@ logger = logging.getLogger(__name__)
 '''
 
 @pytest.fixture
-def gcp_iam_handler_init() -> GcpIAMHandler:
-    gcp_handler = GcpIAMHandler()
+def gcp_iam_handler_init() -> GCPIAMHandler:
+    gcp_handler = GCPIAMHandler()
     return gcp_handler
 
 class TestGCPIAMHandler:
 
     @pytest.mark.asyncio
-    async def test_gcp_iam_handler(self,gcp_iam_handler_init: GcpIAMHandler):
+    async def test_gcp_iam_handler(self,gcp_iam_handler_init: GCPIAMHandler):
         result = await gcp_iam_handler_init.collect_project_iam_evidence()
         logger.info(f"Response {result}")
