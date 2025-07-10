3f3a9aa457e21b8c76185de2de07492ac29022ab
Balamurugan355 <pbalamurugan297@gmail.com>
2025-07-10 19:20:23 +0530

    removed scope

--- superagentx_handlers/gcp/cloud_run.py
+++ superagentx_handlers/gcp/cloud_run.py
@@ -45,23 +45,20 @@ def get_nested_attr(obj: Any, path: List[str], default: Any = None) -> Any:
 class GCPCloudRunHandler(BaseHandler):
     def __init__(
             self,
-            scope: List[str] | None = None,
             creds: str | dict | None = None
     ):
         super().__init__()
-
-        self.scope = scope
 
         # Load credentials from path or dict
         creds = creds or os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
         if isinstance(creds, str):
             credentials = service_account.Credentials.from_service_account_file(
-                creds, scopes=self.scope
+                creds
             )
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = creds
         elif isinstance(creds, dict):
             credentials = service_account.Credentials.from_service_account_info(
-                creds, scopes=self.scope
+                creds
             )
         else:
             raise ValueError("Invalid credentials: must be a file path or a dictionary.")
