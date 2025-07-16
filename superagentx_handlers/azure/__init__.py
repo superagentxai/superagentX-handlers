e2be0ff7b6c9a4262ec973d6374c4c4beefb8060
Balamurugan355 <pbalamurugan297@gmail.com>
2025-07-16 12:09:39 +0530

    renamed

--- superagentx_handlers/azure/__init__.py
+++ superagentx_handlers/azure/__init__.py
@@ -4,5 +4,5 @@ from .loadbalancer import AzureLoadBalancerHandler
 from .nsg import AzureNSGHandler
 from .storage import AzureStorageHandler
 from .vm import AzureVMHandler
-from .api_gateway import AzureApplicationGatewayHandler
+from .application_gateway import AzureApplicationGatewayHandler
 
