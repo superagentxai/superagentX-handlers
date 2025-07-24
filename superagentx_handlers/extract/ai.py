93cb60bbf6653e7d7f58bb9de5f88986c2412268
Vetharupini <vetharupini938@gmail.com>
2025-07-24 10:34:17 +0530

    changes done in the extract handler

--- superagentx_handlers/extract/ai.py
+++ superagentx_handlers/extract/ai.py
@@ -3,33 +3,35 @@ import json
 import logging
 import os
 import time
+import requests
+from typing import Optional
 
-from pathlib import Path
+import aiohttp
 from superagentx.handler.base import BaseHandler
 from superagentx.handler.decorators import tool
-
-import requests
+from functools import lru_cache
 
 logger = logging.getLogger(__name__)
 
 
 class ExtractAIHandler(BaseHandler):
+    API_EXTRACT_ENDPOINT: str = '/extutil/api/v1/do'
+    API_STATUS_ENDPOINT: str = '/extutil/api/v1/status'
 
     def __init__(
             self,
             api_token: str,
             prompt_name: str,
-            extract_status_url: str | None = None,
-            extract_url: str | None = None,
+            base_url: str | None = None,
             project_id: str | None = None
     ):
         super().__init__()
-        self.api_token = api_token or os.getenv("API_TOKEN")
-        self.extract_status_url = extract_status_url or os.getenv("EXTRACT_STATUS_URL")
-        self.extract_url = extract_url or os.getenv("EXTRACT_URL")
+        self.api_token = api_token or os.getenv("EXTRACT_API_TOKEN")
+        self.base_url = base_url or os.getenv("BASE_URL")
         self.prompt_name = prompt_name
         self.project_id = project_id
 
+    @lru_cache(maxsize=1)
     def get_header(self):
         return {
             'api-token': self.api_token,
@@ -38,6 +40,12 @@ class ExtractAIHandler(BaseHandler):
 
     @tool
     async def get_file_base64_data(self, file_path):
+        """
+        Read a file from the given path and return its Base64-encoded content.
+
+        Args:
+            file_path (str): The path to the file that needs to be read.
+        """
         try:
             logger.info(f"Reading file {file_path}")
             # Open the PDF file in binary mode
@@ -50,22 +58,20 @@ class ExtractAIHandler(BaseHandler):
             # print("File not found. Please provide a valid file path.")
             return None
 
-    def get_invoice_json_data(self, reference_id: str):
+    async def get_invoice_json_data(self, reference_id: str):
         logger.info(f"Getting invoice json data for {reference_id}")
-        get_url = f"{self.extract_status_url}/{reference_id}"
+        get_url = f"{self.base_url}/{self.API_STATUS_ENDPOINT}/{reference_id}"
         response = requests.request("GET", get_url, headers=self.get_header())
         return json.loads(response.text)
 
-    def get_payload(self, project_id, file_path, file_data):
-        return {
+    async def process_request(self, post_url, project_id, file_path, file_data):
+        storage_file_path = file_path
+        payload = {
             "project_id": project_id,
-            "file_name_or_path": str(file_path),
+            "file_name_or_path": storage_file_path,
             "file_data": file_data,
             "instruction_type": self.prompt_name
         }
-
-    def process_request(self, post_url, project_id, file_path, file_data):
-        payload = self.get_payload(project_id=project_id, file_data=file_data, file_path=file_path)
         response = requests.post(post_url, headers=self.get_header(), data=json.dumps(payload))
 
         if response.status_code == 200:
@@ -75,7 +81,7 @@ class ExtractAIHandler(BaseHandler):
             raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
 
     @tool
-    async def extract_api(self, file_path: str, file_data, project_id: str | None = None, poll_interval=7):
+    async def extract_api(self, file_path: str, file_data, poll_interval: int, project_id: Optional[str] = None):
         """
         Unified extract method for both DF and SAgentX responses.
         Returns extracted data or None on failure.
@@ -87,8 +93,8 @@ class ExtractAIHandler(BaseHandler):
         if not project_id:
             project_id = self.project_id
 
-        j_res = self.process_request(self.extract_url, project_id=project_id, file_path=file_path, file_data=file_data)
-        # Detect response type
+        j_res = await self.process_request(f"{self.base_url}{self.API_EXTRACT_ENDPOINT}", project_id=project_id,
+                                           file_path=file_path, file_data=file_data)  # Detect response type
         if j_res.get('statusCode') == '200':  # DF style
             job_id = j_res['jobId']
             ref_id = j_res['referenceId']
@@ -105,7 +111,7 @@ class ExtractAIHandler(BaseHandler):
         logger.info(f'Extracting file JobId: {job_id}')
 
         while True:
-            res = self.get_invoice_json_data(ref_id)
+            res = await self.get_invoice_json_data(ref_id)
             if res.get("status") == "FAILED":
                 logger.error(f"Extract FAILED for job: {job_id}, Ref: {ref_id}")
                 return None
@@ -117,3 +123,4 @@ class ExtractAIHandler(BaseHandler):
 
             logger.info(f'Extracting SUCCESS for JobId: {job_id}')
             return res if return_key == 'DF' else res.get('extractJobInfo')
+
---
+++ tests/handlers/extract/test_ai.py
@@ -0,0 +1,41 @@
+import os
+
+import pytest
+import logging
+
+from superagentx_handlers.extract.ai import ExtractAIHandler
+
+logger=logging.getLogger(__name__)
+
+'''
+    1. pytest --log-cli-level=INFO tests/handlers/extract/test_ai.py::TestExtractAI::test_extract_api
+    2. pytest --log-cli-level=INFO tests/handlers/extract/test_ai.py::TestExtractAI::test_extract_status
+
+'''
+
+@pytest.fixture
+def extract_init() -> ExtractAIHandler:
+    extract_handler = ExtractAIHandler(
+        api_token= "" or os.getenv('API_TOKEN'),
+        prompt_name="<PROMPT_NAME>",
+        base_url = "<BASE_URL>",
+    )
+    return extract_handler
+
+class TestExtractAI:
+
+    async def test_extract_api(self, extract_init: ExtractAIHandler):
+        data = await extract_init.get_file_base64_data("")
+        result = await extract_init.extract_api(
+            project_id="<PROJECT_ID>",
+            file_data= data,
+            file_path="<FILE_PATH>",
+            poll_interval=7
+        )
+        logger.info(result)
+
+    async def test_extract_status(self, extract_init: ExtractAIHandler):
+        result = await extract_init.get_invoice_json_data(
+            reference_id="<REFERENCE_ID>"
+        )
+        logger.info(result)
