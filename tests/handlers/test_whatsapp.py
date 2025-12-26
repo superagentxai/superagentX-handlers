44642a6406c42e7ca9f5cb40f0f85a770b2c85f0
VetharupiniSankar <vetharupini@deeplore.io>
2025-12-26 16:48:17 +0530

    changes done in whatsapp test case

--- tests/handlers/test_whatsapp.py
+++ tests/handlers/test_whatsapp.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
  Run Pytest:  
 
     1.pytest --log-cli-level=INFO tests/handlers/test_whatsapp.py::TestWhatsappHandler::test_send_message    
-    pytest --log-cli-level=INFO tests/handlers/test_whatsapp.py::TestWhatsappHandler::test_check_new_attachments   
 
 '''
 
